bfd3dd625d529a661406fa46d1cfd7a0832b7599
tfmijeff <tfmi.jeff@gmail.com>
2025-12-12 16:02:11 +0800

    Update retriever.js

--- rag/retriever.js
+++ rag/retriever.js
@@ -1,15 +1,7 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
-function loadDocuments() {
-  const dir = path.join(__dirname, "..", "knowledge");
-  const files = fs.readdirSync(dir);
-
-  return files.map(file => {
-    const fullPath = path.join(dir, file);
-    return JSON.parse(fs.readFileSync(fullPath, "utf8"));
-  });
-}
+const KNOWLEDGE_DIR = path.resolve("./knowledge");
 
 function tokenize(text) {
   return text
@@ -19,25 +11,23 @@ function tokenize(text) {
     .filter(Boolean);
 }
 
-function scoreDoc(questionTokens, doc) {
-  const docTokens = tokenize(doc.title + " " + doc.content);
-  const hit = questionTokens.filter(t => docTokens.includes(t));
-  return hit.length / questionTokens.length;
-}
-
-function retrieve(question, topK = 2) {
-  const docs = loadDocuments();
+export function retrieve(question, topK = 2) {
   const qTokens = tokenize(question);
 
-  const scored = docs.map(doc => ({
-    doc,
-    score: scoreDoc(qTokens, doc)
-  }));
+  const files = fs.readdirSync(KNOWLEDGE_DIR);
+  const docs = files.map(f =>
+    JSON.parse(fs.readFileSync(path.join(KNOWLEDGE_DIR, f), "utf8"))
+  );
+
+  const scored = docs.map(doc => {
+    const dTokens = tokenize(doc.title + " " + doc.content);
+    const hit = qTokens.filter(t => dTokens.includes(t));
+    const score = hit.length / qTokens.length;
+    return { doc, score };
+  });
 
   return scored
     .filter(r => r.score > 0)
     .sort((a, b) => b.score - a.score)
     .slice(0, topK);
 }
-
-module.exports = { retrieve };
